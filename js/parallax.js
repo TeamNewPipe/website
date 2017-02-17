c6793be9ba44bdf6f5420324d950f24046b3efe2
TobiGr <tooobi14@gmail.com>
2017-02-17 22:00:01 +0100

    Improve performance and design (parallax)

--- js/parallax.js
+++ js/parallax.js
@@ -1,24 +1,35 @@
+var headerHeight, featuresHeight, featuresPosition;
 
-var headerHeight = $('.header-wrapper').outerHeight();
-$('.header-background').css('height', headerHeight + 'px');
-var featuresHeight = $('.features-wrapper').outerHeight();
-var featuresPosition = $('.features-wrapper').position().top;
-$('.features-background').css('height', featuresHeight + 'px');
-function parallax(){
+function reset() {
+    headerHeight = $('.header-wrapper').outerHeight();
+    /* check wheter height can encrease when navbar can be (un-)collapsed */
+    if($(window).width() > 767){
+        $('.header-background').css('height', headerHeight + 'px');
+    }
+    else {
+        $('.header-background').css('height', '100vh');
+    }
+    featuresHeight = $('.features-wrapper').outerHeight();
+    featuresPosition = $('.features-wrapper').position().top;
+    $('.features-background').css('height', featuresHeight + 'px');
+}
+
+function parallax() {
     var scrolled = $(window).scrollTop();
     $('.header-background').css('height', (headerHeight - scrolled) + 'px');
     $('.features-background').css('height', (featuresHeight - scrolled + featuresPosition) + 'px');
 }
 
-$(window).scroll(function(e){
+$(window).scroll(function(e) {
     if($(window).width() > 767){
         parallax();
     }
 });
 
 $(window).resize(function(e) {
-    featuresPosition = $('.features-wrapper').position().top;
+    reset();
     parallax();
 });
 
+reset();
 parallax();
