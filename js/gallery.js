42cbba4e9cc81bcf4f51e2fba1f8cc5cf103a6e8
TobiGr <TobiGr@users.noreply.github.com>
2017-06-14 15:28:23 +0200

    Improve feature tiles for extra small devices (phones portrait) Disable gallery, show only first slide

--- js/gallery.js
+++ js/gallery.js
@@ -12,17 +12,19 @@ $(document).ready(function(){
         speed: 1000,
         mode: 'horizontal',
         responsive: true,
-        adaptiveHeight: false
+        adaptiveHeight: false,
+        controls: false,
+        touchEnabled: false
     });
     
     /* do not display slides at small screens that are only used for large screens */
     el = $('.gallery-element-wrapper.gallery-desktop-item');
     comment = document.createComment(el.get(0).outerHTML);
     lastWindowWidth = $(window).width();
-    if(lastWindowWidth <= 767){
+    /*if(lastWindowWidth <= 767){
         el.replaceWith(comment);
         slider.reloadSlider();
-    }
+    }*/
     
     $(window).on("orientationchange load resize", function () {
         gallery_resize();
@@ -30,14 +32,16 @@ $(document).ready(function(){
 });
 
 function gallery_slide(s){
+    /* TODO: https://github.com/TeamNewPipe/website/issues/22 */
+    return;
     slider.goToSlide(s);
 }
 
 function gallery_resize(){
     el = $('.gallery-element-wrapper.gallery-desktop-item');
-    
+    /* TODO: https://github.com/TeamNewPipe/website/issues/22 */
     /* small screens */
-    if($(window).width() <= 767 && lastWindowWidth > 767){
+    /*if($(window).width() <= 767 && lastWindowWidth > 767){
         lastWindowWidth = $(window).width();
         el.replaceWith(comment);
         slider.reloadSlider({
@@ -47,7 +51,7 @@ function gallery_resize(){
     }
     
     /* medium and large screens */
-    else if($(window).width() > 767 && lastWindowWidth <= 767) {
+   /* else if($(window).width() > 767 && lastWindowWidth <= 767) {
         lastWindowWidth = $(window).width();
         $(comment).parent().prepend(comment.nodeValue);
         $(comment).remove();
@@ -57,9 +61,39 @@ function gallery_resize(){
     }
     
     /* prevent template's bug, which does not allow responsive behavior in some browsers */
-    else {
+    //else {
         slider.reloadSlider({
-            startSlide:slider.getCurrentSlide()
+            controls: false,
+            startSlide:slider.getCurrentSlide(),
+            touchEnabled: false
         });
+    //}
+}
+
+/* Bootstrap 3 collapse function has some bugs wich cause ugly toggles */
+var visibleDescription = null;
+
+function collapseDescription(dest) {
+    if($(window).width() <= 543) {
+        if($(dest).css('display') == 'none'){
+            $('.text .description-text').hide();
+            $(dest).show();
+            visibleDescription = $(dest);
+        }
+        else {
+            $(dest).hide();
+            visibleDescription = null;
+        }
     }
 }
+$(window).on("load orientationchange resize", function () {
+    if($(window).width() > 543){
+        $('.text .description-text').show();
+    }
+    else {
+        $('.text .description-text').hide();
+        visibleDescription = null;
+    }
+    if(visibleDescription != null) 
+        $(visibleDescription).show();;
+});
