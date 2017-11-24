c9324f350ba97cafb9b968b036759c7f9cf27cee
TobiGr <TobiGr@users.noreply.github.com>
2017-11-24 13:41:55 +0100

    more content design fixes

--- js/mediaview.js
+++ js/mediaview.js
@@ -4,3 +4,6 @@ function viewMediaFile(m) {
     img.src = (m.getAttribute("src") != null && m.getAttribute("src") != 'undefined') ? m.getAttribute("src") : m.getElementsByTagName("img")[0].getAttribute("src");
     //alert(m.getAttribute("src"));
 }
+$('#mediaFileView').on('show.bs.modal', function (e) {
+      if($(e.relatedTarget).hasClass('media-download')) e.stopPropagation();
+});
