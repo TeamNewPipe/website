48e01c22cac3eba7bf6a20a09a9fec4830cfd878
TobiGr <TobiGr@users.noreply.github.com>
2017-12-01 17:08:40 +0100

    small fixes
    fix modal opening when download link clicked

--- js/mediaview.js
+++ js/mediaview.js
@@ -1,9 +1,13 @@
+// set the media to display in #mediaFileView
 function viewMediaFile(m) {
-    //document.getElementsByTagName("HTML")[0].setAttribute("class", "modal-open-a");
     var img = document.getElementById("mediaFullScreen");
     img.src = (m.getAttribute("src") != null && m.getAttribute("src") != 'undefined') ? m.getAttribute("src") : m.getElementsByTagName("img")[0].getAttribute("src");
-    //alert(m.getAttribute("src"));
 }
+
+// prevent opening #mediaFileView (modal) when a download link is clicked
 $('#mediaFileView').on('show.bs.modal', function (e) {
-      if($(e.relatedTarget).hasClass('media-download')) e.stopPropagation();
+  var button = e.relatedTarget;
+  if($(button).hasClass('media-download')) {
+    e.stopPropegation();
+  }  
 });
