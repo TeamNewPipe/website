77f511203b0e008e869ae56969ccfcdb8c027522
TobiGr <tobigr@users.noreply.github.com>
2018-09-17 20:37:36 +0200

    Add Git ID to footer
    Closes #80

---
+++ _plugins/git_hash.rb
@@ -0,0 +1,14 @@
+module Jekyll
+
+    #
+    # Show the hash of HEAD.
+    #
+    class GitHash < Liquid::Tag
+        include Jekyll::LiquidExtensions
+		def render(context)
+            %x( git rev-parse --short HEAD )
+		end
+    end
+end
+
+Liquid::Template.register_tag('git_hash', Jekyll::GitHash)
