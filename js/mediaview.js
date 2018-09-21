efcfab1bd99f8e34ccc1b10252b4d8e4eb884306
TobiGr <tobigr@users.noreply.github.com>
2018-09-21 19:11:28 +0200

    Add video support to fullscreen mediaView

--- js/mediaview.js
+++ js/mediaview.js
@@ -1,19 +1,37 @@
 /**
- * Handle fullsize images with Bootstrap modal
+ * Handle full-size images with Bootstrap modal
  **/
+let img, video;
 
 // set the media to display in #mediaFileView
 function viewMediaFile(media, openModal = false) {
-    console.log(media);
-    var img = document.getElementById("mediaFullScreen");
-    var attr = media.getAttribute("src");
-    img.src = (attr != null && attr != 'undefined') ? attr : media.getElementsByTagName("img")[0].getAttribute("src");
-    if(openModal) $('#mediaFileView').modal('show');
+    if (img === undefined || img === "undefined" || img === null) {
+        img = document.getElementById("mediaFullScreen");
+        video = document.getElementById("videoFullScreen");
+    }
+
+    let attr = media.getAttribute("src");
+
+    switch (media.tagName) {
+        case "img":
+        case "IMG":
+            img.src = (attr != null && attr !== 'undefined') ? attr : media.getElementsByTagName("img")[0].getAttribute("src");
+            break;
+
+        case "video":
+        case "VIDEO":
+            video.src = (attr != null && attr !== 'undefined') ? attr : media.getElementsByTagName("video")[0].getAttribute("src");
+            video.play();
+            video.loop = !!media.loop;
+            break;
+    }
+    
+    if (openModal) $('#mediaFileView').modal('show');
 }
 
 // prevent opening #mediaFileView (modal) when a download link is clicked
 $('#mediaFileView').on('show.bs.modal', function (e) {
-  var button = e.relatedTarget;
+  let button = e.relatedTarget;
   if ($(button).hasClass('media-download') || $(button).hasClass('author')) {
     e.stopPropegation();
   }  
@@ -21,7 +39,7 @@ $('#mediaFileView').on('show.bs.modal', function (e) {
 
 // close #mediaFileView (modal) with escape key
 $(document).keyup(function(e) {
-     if (e.keyCode == 27) { // escape key := keycode `27`
+     if (e.keyCode === 27) { // escape key := keycode `27`
         $('#mediaFileView').modal('hide');
     }
 });
